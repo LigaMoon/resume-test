e19f5b2e7570210d738f9915c70ef4ad2e155ec6
liigalized <liga.baikova@gmail.com>
2020-07-25 08:28:22 +0000

    fix repo display bug

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,7 +16,7 @@ function userInformationHTML(user){
 }
 
 function repoInformationHTML(repos){
-    if(repos.lentgh === 0) {
+    if(repos.length === 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
@@ -36,24 +36,27 @@ function repoInformationHTML(repos){
             </div>`;
 }
 
-function fetchGitHubInformation(){
+function fetchGitHubInformation(event){
+    $("#gh-user-data").html("");
+    $("#gh-user-repo").html("");
+
     var username = $("#gh-username").val();
     if(!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub Username</h2>`)
         return;
     }
 
-    $("#gh-user-data").html(`<div id="loader"><img src="./assets/css/loader.gif" alt="loading..."></div>`)
+    $("#gh-user-data").html(`<div id="loader"><img src="./assets/css/loader.gif" alt="loading..."></div>`);
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
+        function(firstResponse, secondResponse) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
-            $("#gh-user-data").html(userInformationHTML(userData))
-            $("#gh-user-data").html(repoInformationHTML(repoData))
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for the user ${username}</h2>`);
@@ -63,3 +66,5 @@ function fetchGitHubInformation(){
             }
         });
 }
+
+$(document).ready()
